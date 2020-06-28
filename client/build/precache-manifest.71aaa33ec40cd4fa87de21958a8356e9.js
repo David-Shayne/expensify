7f466ff3f24ad9d0206f165b084701ca8215c4fc
unknown <davidmshayne.jftd@gmail.com>
2020-06-28 20:46:22 +0200

    .

---
+++ client/build/precache-manifest.71aaa33ec40cd4fa87de21958a8356e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c341ed9fe95c2c123540786504e9193",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9d2d230f5538e5a468df",
+    "url": "./static/css/main.2eca0582.chunk.css"
+  },
+  {
+    "revision": "b90e67ad78894f24d8a0",
+    "url": "./static/js/2.f105f623.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.f105f623.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d2d230f5538e5a468df",
+    "url": "./static/js/main.d29cafa5.chunk.js"
+  },
+  {
+    "revision": "d90c5b93796701d5570a",
+    "url": "./static/js/runtime-main.8b0ae790.js"
+  }
+]);
