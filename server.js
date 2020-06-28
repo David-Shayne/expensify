e15b6d2d5ca27511bb47429e6b51932106ba8f18
unknown <davidmshayne.jftd@gmail.com>
2020-06-28 18:45:17 +0200

    .

---
+++ server.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`server started on port ${PORT}...`));
